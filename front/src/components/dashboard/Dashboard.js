df6289e5c8e0977ec20da216c0774ff485683fee
Signoraelle <alicecasadei@icloud.com>
2020-04-14 05:04:12 +0200

    up

--- front/src/components/dashboard/Dashboard.js
+++ front/src/components/dashboard/Dashboard.js
@@ -3,14 +3,14 @@
 
 import React, { Component } from 'react'
 
-import ProjectList from '../projects/ProjectList'
+// import ProjectList from '../projects/ProjectList'
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            {/* <ProjectList /> */}
             <div className="col s12 m5 offset-m1">
 
             </div>

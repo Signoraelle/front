ab0a4ec42b84ca8c6983e97fd079e0b96ef872f0
Signoraelle <alicecasadei@icloud.com>
2020-04-14 05:01:43 +0200

    up

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/layout/Navbar";
-import ProjectDetails from './components/projects/ProjectDetails';
+// import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from "./components/auth/SingIn";
 import SignUp from './components/auth/SingUp'
-import CreateProject from './components/projects/CreateProject'
+// import CreateProject from './components/projects/CreateProject'
 class App extends Component {
   render() {
     return (
@@ -15,10 +16,10 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
 
-            <Route path='/project/:id' component={ProjectDetails} />
+            {/* <Route path='/project/:id' component={ProjectDetails} /> */}
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
-            <Route path='/create' component={CreateProject} />
+            {/* <Route path='/create' component={CreateProject} /> */}
           </Switch>
         </div>
       </BrowserRouter >
--- front/src/components/dashboard/Dashboard.js
+++ front/src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@
 
 
 import React, { Component } from 'react'
-import Notifications from './Notifications'
+
 import ProjectList from '../projects/ProjectList'
 class Dashboard extends Component {
   render() {
@@ -12,7 +12,7 @@ class Dashboard extends Component {
           <div className="col s12 m6">
             <ProjectList />
             <div className="col s12 m5 offset-m1">
-              <Notifications />
+
             </div>
           </div>
         </div>

062cbacede7e1d64c5a8aed9b54875a44e242d78
Signoraelle <alicecasadei@icloud.com>
2020-04-14 19:24:26 +0200

    Quest 040

--- back/app.js
+++ back/app.js
@@ -11,15 +11,16 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.use("/auth", authRouter);
-// implement the API part
-app.get("/", (req, res) => {
-  res.send("youhou");
-})
+app.use(express.static(__dirname + "/public"));
+
+app.use("/auth/signup", require("./routes/auth/auth"));
+
+app.get("/auth/signup", (req, res) => {
+  res.send("I am in POST signup");
+});
 /// in case path is not found, return the 'Not Found' 404 code
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,19 +1,15 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
 
-// define the home page route
-router.get("/", function (req, res) {
-  res.send("Home page for auth");
-});
+const router = express.Router();
+const app = express();
 
-router.post("/signup", function (req, res, next) {
-  /*   res.send('I am in POST signup'); */
-  res.send("I am in POSt Signup");
-});
 
-router.post("/signin", function (req, res, next) {
-  /*   res.send('I am in POST signup'); */
-  res.send("I am in POSt Signin");
+const connection = require("../../helpers/db");
+
+router.post("/", (req, res) => {
+  connection.query("INSERT INTO users SET ?", req.body, (err, result) => {
+    res.send(result);
+  });
 });
 
 module.exports = router;
